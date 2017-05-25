902c06eff9920874472772ca3a68f0783704b191
Dennis <d.kao@live.com>
2017-05-25 17:02:03 -0400

    Fixed a typo

--- Analysis/NormalizeAndDiscoverNovelJunctions.sh
+++ Analysis/NormalizeAndDiscoverNovelJunctions.sh
@@ -13,6 +13,8 @@
 
 beryl_home=~/tools/MendelianRNA-seq
 
+echo "Running NormalizeAndDiscoverNovelJunctions.sh"
+
 #	Mandatory parameters
 if [ -z "$input" ];
 	then
@@ -52,3 +54,5 @@ outputFilePath=`dirname $input`
 output="threshold"$threshold"_novel_"$sample"_norm_"$inputFileName
 
 $beryl_home/Analysis/NormalizeSpliceJunctionValues.py -transcript_model=$transcript_model -splice_file=$input --normalize | grep $sample | grep -v '*' | awk "{ if (\$5 == 1 && \$4 >= $minread ) print \$0 }" | sed "s/:$sample//" | sed "s/:$sample//" | awk "{if (\$7 == \"Neither\" || \$9 > $threshold) print \$0}" | cut -f 1,2,3,4,7,8,9 > $outputFilePath/$output
+
+echo "NormalizeAndDiscoverNovelJunctions.sh has finished running"
