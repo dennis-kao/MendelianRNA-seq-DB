dfde1ffbc1ce0971215b5251bcdbd6bc93b36893
Dennis <d.kao@live.com>
2017-05-26 10:51:40 -0400

    Changed around how transcript_model is handled

--- Analysis/NormalizeAndDiscoverNovelJunctions.sh
+++ Analysis/NormalizeAndDiscoverNovelJunctions.sh
@@ -42,9 +42,11 @@ then
 		threshold=0.5
 fi
 
+transcript_model_arg='transcript_model='$transcript_model
+
 if [ -z "$transcript_model" ];
 then
-		transcript_model="$beryl_home/gencode.comprehensive.splice.junctions.txt"
+		transcript_model_arg=''
 fi
 
 inputFileName=`basename $input`
@@ -53,6 +55,6 @@ outputFilePath=`dirname $input`
 #	Actual code
 output="threshold"$threshold"_novel_"$sample"_norm_"$inputFileName
 
-$beryl_home/Analysis/NormalizeSpliceJunctionValues.py -transcript_model=$transcript_model -splice_file=$input --normalize | grep $sample | grep -v '*' | awk "{ if (\$5 == 1 && \$4 >= $minread ) print \$0 }" | sed "s/:$sample//" | sed "s/:$sample//" | awk "{if (\$7 == \"Neither\" || \$9 > $threshold) print \$0}" | cut -f 1,2,3,4,7,8,9 > $outputFilePath/$output
+$beryl_home/Analysis/NormalizeSpliceJunctionValues.py $transcript_model_arg -splice_file=$input --normalize | grep $sample | grep -v '*' | awk "{ if (\$5 == 1 && \$4 >= $minread ) print \$0 }" | sed "s/:$sample//" | sed "s/:$sample//" | awk "{if (\$7 == \"Neither\" || \$9 > $threshold) print \$0}" | cut -f 1,2,3,4,7,8,9 > $outputFilePath/$output
 
 echo "NormalizeAndDiscoverNovelJunctions.sh has finished running"
