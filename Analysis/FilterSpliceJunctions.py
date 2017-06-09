8d7f77b66f7349ac918ce849c52bd34072ee0af4
Dennis Kao <d.kao@live.com>
2017-06-09 15:48:03 -0400

    Fixed a string. DOH

--- Analysis/FilterSpliceJunctions.py
+++ Analysis/FilterSpliceJunctions.py
@@ -35,7 +35,7 @@ if __name__=="__main__":
 
 	parser = argparse.ArgumentParser(description = 'Filter out splice junctions based on read count, normalized read count and specificity to a sample')
 	parser.add_argument('-file',help="The path to the file produced by NormalizeSpliceJunctionValues.py")
-	parser.add_argument('-sample',help="The name of the bam file you want to discover novel junctions in without the /".bam/" extension")
+	parser.add_argument('-sample',help="The name of the bam file you want to discover novel junctions in without the \".bam\" extension")
 	parser.add_argument('-minRead',help="The minimum read count a junction needs to have to be considered, default=10", default=10)
 	parser.add_argument('-minNormRead',help="The minimum normalized read count a junction needs to have to be considered, default=0.5", default=0.5)
 	args=parser.parse_args()
